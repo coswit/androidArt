4601b3ab5fdace5fde5754ccbef0c1d12ce73754
zhj <zhengjingytz@126.com>
2019-08-31 17:14:28 +0800

    threadLoacal

--- chapter3/src/main/java/com/ryg/chapter_3/chapter_3/MainActivity.java
+++ chapter3/src/main/java/com/ryg/chapter_3/chapter_3/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.drawable.ClipDrawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -11,6 +12,9 @@ import com.ryg.chapter_3.R;
 
 public class MainActivity extends Activity {
 
+    private ThreadLocal<Boolean> mBooleanThreadLocal = new ThreadLocal<Boolean>();
+
+    private String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,12 +23,15 @@ public class MainActivity extends Activity {
         ImageView testClip = (ImageView) findViewById(R.id.test_clip);
         ClipDrawable testClipDrawable = (ClipDrawable) testClip.getDrawable();
         testClipDrawable.setLevel(5000);
+
+
     }
 
     public void onButtonClick(View v) {
         if (v.getId() == R.id.button1) {
-            Intent intent = new Intent(this, TestActivity.class);
-            startActivity(intent);
+//            Intent intent = new Intent(this, TestActivity.class);
+//            startActivity(intent);
+           testThreadLocal();
         } else if (v.getId() == R.id.button2) {
             Intent intent = new Intent(this, DemoActivity_1.class);
             startActivity(intent);
@@ -32,5 +39,28 @@ public class MainActivity extends Activity {
             Intent intent = new Intent(this, DemoActivity_2.class);
             startActivity(intent);
         }
+
+    }
+
+    private void testThreadLocal(){
+        mBooleanThreadLocal.set(true);
+        Log.d(TAG,"[Thread#main]mBooleanThreadLocal=" + mBooleanThreadLocal.get());
+
+
+        new Thread("Thread#1") {
+            @Override
+            public void run() {
+                mBooleanThreadLocal.set(false);
+                Log.d(TAG,"[Thread#1]mBooleanThreadLocal=" + mBooleanThreadLocal.get());
+            };
+        }.start();
+
+
+        new Thread("Thread#2") {
+            @Override
+            public void run() {
+                Log.d(TAG,"[Thread#2]mBooleanThreadLocal=" + mBooleanThreadLocal.get());
+            };
+        }.start();
     }
 }
